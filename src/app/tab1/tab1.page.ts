1120a4f7d742d73ffd1925539ce9ea59ed0aa162
Blen6035 <akhaberman@lcmail.lcsc.edu>
2019-04-01 21:17:51 -0700

    Slider Tab 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,3 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {}
+
+@Component({
+  selector: 'slides-example',
+  template: `
+    <ion-slides pager="true" [options]="slideOpts">
+      <ion-slide>
+        <ion-img src="/assets/arches-national-park-dark-dusk-33688.jpg"></ion-img>
+      </ion-slide>
+      <ion-slide>
+        <ion-img src="/assets/4k-wallpaper-conifers-desktop-wallpaper-1367192.jpg"></ion-img>
+      </ion-slide>
+      <ion-slide>
+        <ion-img src="/assets/arches-national-park-arizona-canyon-1672813.jpg"></ion-img>
+      </ion-slide>
+    </ion-slides>
+  `
+})
+export class SlideExample {
+  slideOpts = {
+    effect: 'flip'
+  };
+  constructor() {}
+}
